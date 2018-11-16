4b9a9c0e764d50dcd8d3f37d535bcaa833bd4371
279611480 <279611480@qq.com>
2018-11-16 11:03:12 +0800

    day06   07--文件下载

--- file/src/main/java/org/yun/storage/service/StorageService.java
+++ file/src/main/java/org/yun/storage/service/StorageService.java
@@ -1,8 +1,10 @@
 package org.yun.storage.service;
 
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 import org.springframework.data.domain.Page;
+import org.yun.common.data.domain.Result;
 import org.yun.identity.domain.User;
 import org.yun.storage.domain.FileInfo;
 
@@ -13,4 +15,11 @@ public interface StorageService {
 	
 	void save(FileInfo info, InputStream in);
 	Page<FileInfo> findFiles(String keyword, Integer number);
+	
+	FileInfo finById(String id);
+
+	InputStream getFileContent(FileInfo fi) throws FileNotFoundException;
+	
+	Result deleteFile(String id);
+	
 }
